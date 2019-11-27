29a1dccd88ce5c69ded06ef633c038fdd5847833
vapvin <tpehdj12@daum.net>
2019-11-27 16:47:57 +0900

    Main Ts Error ㅠ

---
+++ main.js
@@ -0,0 +1,11 @@
+// type script is so Good;
+// typecheck
+var Card = /** @class */ (function () {
+    function Card(num, sim) {
+    }
+    Card.prototype.turn = function () {
+        while ()
+            ;
+    };
+    return Card;
+}());

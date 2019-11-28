0e3913990e759ef39bec4980adc056083dea07d9
vapvin <tpehdj12@daum.net>
2019-11-28 13:40:38 +0900

    Restudy Python

--- main.py
+++ main.py
@@ -72,3 +72,24 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGING_EMAIL
     )
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={'pk': self.pk})
+
+    def verify_email(self):
+        if self.email_verified is False:
+            secret = uuid.uuid4().hex[:20]
+            self.email_secret = secret
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
+            send_mail(
+                "Verify Airbnb Account",
+                strip_tags(html_message),
+                settings.EMAIL_FROM,
+                [self.email],
+                fail_silently=False,
+                html_message=html_message,
+            )
+            self.save()
+        return

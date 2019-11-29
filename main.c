81d59a8d9aeb3096c538e3a3e0013d4cdbe358b0
vapvin <tpehdj12@daum.net>
2019-11-29 14:49:55 +0900

    new block

--- main.c
+++ main.c
@@ -378,3 +378,41 @@ void draw_main(void)
         }
     }
 }
+
+void new_block(void)
+{
+    int i, j;
+
+    bx = (MAIN_X / 2) - 1;
+    by = 0;
+    b_type = b_type_next;
+    b_type_next = rand() % 7;
+    b_rotation = 0;
+
+    new_block_on = 0;
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            if (blocks[b_type][b_rotation][i][j] == 1)
+                main_org[by + i][bx + j] = ACTIVE_BLOCK;
+        }
+    }
+    for (i = 1; i < 3; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            if (blocks[b_type_next][0][i][j] == 1)
+            {
+                gotoxy(STATUS_X_ADJ + 2 + j, i + 6);
+                printf("■");
+            }
+            else
+            {
+                gotoxy(STATUS_X_ADJ + 2 + j, i + 6);
+                printf("  ");
+            }
+        }
+    }
+}

d36d1b7e8c79f7ff2db358baab041d8c4a6e5298
vapvin <tpehdj12@daum.net>
2019-12-09 18:04:43 +0900

    Assembly Pyramid Creat

---
+++ pyramid.s
@@ -0,0 +1,24 @@
+section .data
+    STAR db "*"
+    EMPTY db 0x0a
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
+    mov r10, 0
+    mov r9, [rsp + 16]
+
+    cmp r9, 0
+    je _done
+
+    mov cl, [r9]
+    movzx r9, cl
+    sub r9, 0x30
+
+    mov r8, r9
+    xor r9, r9
+    call _syscall

64d88403e34db67c6abf6ae9caf458e83c3856f8
vapvin <tpehdj12@daum.net>
2019-12-23 18:40:55 +0900

    Struct Chain List

--- list.c
+++ list.c
@@ -1,38 +1,16 @@
 #include <stdio.h>
 #define INF 10000
 
-int arr[INF];
+typedef struct {
+    int data;
+    Node *next;
+} Node;
 
-int count = 0;
-
-void addBack(int data) {
-    arr[count] = data;
-    count++;
-}
-
-void addFirst(int data) {
-    for(int i = count; i>=1; i--) {
-        arr[i] = arr[i-1];
-    }
-    arr[0] = data;
-    count++;
-}
-
-int show() {
-    for(int i = 0; i < count; i++) {
-        printf("%d", arr[i]);
-    }
-}
+Node *head;
 
 int main(void) {
 
-    addBack(8);
-    addBack(5);
-    addBack(7);
-    addBack(1);
-    addFirst(2);
-    addFirst(4);
-    show();
+  
     
     return 0;
 }

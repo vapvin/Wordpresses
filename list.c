cf4a8b15157ec5648728af9c90def8d531bbc86a
vapvin <tpehdj12@daum.net>
2019-12-23 18:49:26 +0900

    Next Node To Data

--- list.c
+++ list.c
@@ -10,7 +10,20 @@ Node *head;
 
 int main(void) {
 
-  
+    head = (Node*)malloc(sizeof(Node));
+    Node *node1 = (Node*)malloc(sizeof(Node));
+    node1->data = 1;
+    Node *node2 = (Node*)malloc(sizeof(Node));
+    node2->data = 2;
+    head -> next = node1;
+    node1 -> next = node2;
+    node2 -> next = NULL;
+    Node *cur = head -> next;
+    while(cur != NULL) {
+        printf("%d", cur->data);
+        cur = cur -> next;
+    }
+    
     
     return 0;
 }
